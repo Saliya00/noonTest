995e741b8c1995b7decd3fc37427903f859b2bad
SALIYA PERERA <saliya@SALIYAs-MacBook-Pro.local>
2019-08-23 10:47:10 +0530

    Minor code changes

--- noon.test2.js
+++ noon.test2.js
@@ -1,19 +1,25 @@
 
 describe('My first cypress test',() => {
 
-    // Navigate to website
     it('Navigate to noon website', () => {
+
+        /* 
+        Navigate to website 
+        */
         cy.visit('https://www.noon.com')
         Cypress.Cookies.debug(true)
     
-
-    // Go to Login window and click on Signin button
+        /* 
+        Go to Login window and click on Signin button
+        */
         cy.wait(8000)
         cy.get('#dd_header_signInOrUp').click()
         cy.wait(2000)
         cy.get('#btn_header_signin').click()
 
-    // Enter Signin details and click on Signin button
+        /* 
+        Enter Signin details and click on the Signin button
+        */
         cy.wait(3000)
         cy.get('#tf_signin_email').type('perera_saliya@yahoo.com')
         cy.wait(2000)
@@ -21,48 +27,63 @@ describe('My first cypress test',() => {
         cy.wait(2000)
         cy.get('#btn_signin_signin').click()
    
-
-    // Headover to Electronics section from the top Nav bar
+        /* 
+        Headover to Electronics section from the top Nav bar
+        */
         cy.wait(2000)
         cy.get('.mainCategories').contains('Electronics').click({force:true})
-        cy.wait(20000)
+        cy.wait(15000)
 
-    // Click on the first item that shows up under Electronics which is the '0'th index in swiper-wrapper class
-    // A time delay of around 10s had to be forced to let the items load completely
-        
+        /* Click on the first item that shows up under Electronics which is the '0'th index in swiper-wrapper class
+        A time delay of around 10s had to be forced to let the items load completely 
+        */
         cy.get('.swiper-wrapper').eq(1).children(1).eq(0).click()
 
-    // Add product to cart
+        /* 
+        Add product to cart
+        */
         cy.wait(3000)
         cy.get('.addToCartLabel').click()
         cy.wait(3000)
         cy.get('.addedToCartLabel').eq(1).click()
 
-    // Go to checkout
+        /* 
+        Go to checkout 
+        */
         cy.wait(3000)
         cy.get('.invoiceColumn').click() 
    
-
-    // Enter new address
-   
+        /* 
+        Enter new location
+        In google map search box a location will be typed and and a keypress with the "ENTER" command 
+        will be simulated by the type('{enter}') function.
+        This will execute click on the first search result
+        */
         cy.get('#searchBox').type('United Arab Emirates Honey Shop - Dubai - United Arab Emirates')
         cy.wait(3000)
-        // cy.get('.suggestionsCtr').first().type('{downarrow}{enter}')
-        // cy.get('.suggestionsCtr').trigger("keydown", { key: "enter" });
-        // cy.type('{enter}').trigger('input')
-        // cy.get('#btn_signin_signin').click()
         cy.get('#searchBox').type('{enter}')
-        cy.wait(10000)
+        cy.wait(5000)
         cy.get('.confirmBtn').click()
 
-        // cy.get('.group').eq(0).type('perera_saliya@yahoo.com')
+        /* 
+        Address, first name, last name and phone will be typed on to the relevant input fields 
+        */
         cy.get('input[name="address"]').type('6th lane')
         cy.get('input[name="phone"]').type('2222414')
         cy.get('input[name="firstName"]').type('Saliya')
         cy.get('input[name="lastName"]').type('Perera')
         cy.wait(3000)
+        
+        /* 
+        Click on the "Save Address" button
+         */
         cy.get('.ripple').click()
         cy.wait(5000)
+
+        /* 
+        Click on the "Continue" button and this will take you to
+        the phone number verification page and the test will end
+         */
         cy.get('.ripple').click()
 
 
